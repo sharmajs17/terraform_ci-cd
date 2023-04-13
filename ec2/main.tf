1784dee73b04c23635816d69a62a6998571823a0
Jyotshna Sharma <70006520+sharmajs17@users.noreply.github.com>
2023-04-13 18:12:08 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,13 +6,13 @@ resource "tls_private_key" "key" {
 
 resource "aws_s3_bucket_object" "private_key" {
   bucket = "qwerty12345asdfg"
-  key    = "key1.pem"
+  key    = "key.pem"
   content = tls_private_key.key.private_key_pem
 } 
 
 # Generate a Private Key and encode it as PEM.
 resource "aws_key_pair" "key_pair" {
-  key_name   = "key1"
+  key_name   = "key"
    public_key = tls_private_key.key.public_key_openssh
 
 #  provisioner "local-exec" {
