baf8555134006471915441744ac3798f5eb83d8b
sharmajs17 <jyotshnasharma17@gmail.com>
2023-04-13 12:49:12 +0530

    now it will work

--- ec2/main.tf
+++ ec2/main.tf
@@ -24,7 +24,7 @@ resource "aws_key_pair" "key_pair" {
 # Create a EC2 Instance (Ubuntu 20)
 resource "aws_instance" "example" {
   instance_type          = "t2.micro" # free instance
-  ami                    = "ami-0aa2b7722dc1b5612"
+  ami                    = "ami-007855ac798b5175e"
   key_name               = aws_key_pair.key_pair.id
   vpc_security_group_ids = [var.public_sg]
   subnet_id              = var.public_subnet
